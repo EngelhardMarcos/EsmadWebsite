8756dfc4da8fd95b94021dd7cc8f0f6d8d1cc784
Marcos Engelhard <engelhardmarcos2015@gmail.com>
2023-01-02 17:14:19 +0000

    Added homepage, and something else

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,28 @@
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 
 const header = document.getElementById('header')
-const card = document.querySelector('.card');
+const card = document.querySelectorAll('.card');
 const firstTerm = document.getElementById('FirstTermbtn')
 const secondTerm = document.getElementById('SecondTermbtn')
+const competenciatxt = document.getElementById('competenciatxt')
+let CompetenciaBtn = []
 
-if (card != null) {
-    card.addEventListener('mouseover', function(){
+
+if (card != null && card.length > 0) {
+    for (let i = 0; i < card.length; i++) {
+        card[i].addEventListener('mouseover', function(){
     
-        card.querySelector('.card__inner').classList.add('is-flipped')
+            card[i].querySelector('.card__inner').classList.add('is-flipped')
+            
+        })
         
-    })
+        card[i].addEventListener('mouseout', function(){
+            
+            card[i].querySelector('.card__inner').classList.remove('is-flipped')
+        })
+        
+    }
     
-    card.addEventListener('mouseout', function(){
-        
-        card.querySelector('.card__inner').classList.remove('is-flipped')
-    })
 }
 
 
@@ -42,9 +49,66 @@ window.onscroll = function() {
     }
 
 }
+GetCompetenciaButtons();
+function GetCompetenciaButtons() {
+    
+    for (let i = 0; i < 8; i++) {
+        let button = document.getElementById(`Competencia${i}`)
+        if (button != null) { //Prevent add null objects
+            CompetenciaBtn.push(document.getElementById(`Competencia${i}`))
+        }
+        
+        
+    }
+    for (let index = 0; index < CompetenciaBtn.length; index++) {
+        CompetenciaBtn[index].addEventListener('click', () => CompetenciaButton(CompetenciaBtn[index]))
+        CompetenciaBtn[index].addEventListener('mouseenter', () => Hover(CompetenciaBtn[index]))
+        CompetenciaBtn[index].addEventListener('mouseleave', () => NotHover(CompetenciaBtn[index]))
+    }
+    
+}
+
+function Hover(e) {
+    let img = e.firstElementChild;
+    if (!img.src.includes('Clicked.png')) {
+        img.src = img.src.replace('.png', 'Clicked.png')
+    }
+    
+}
+
+function NotHover(e) {
+    let img = e.firstElementChild;
+    var ret= img.src.replace('Clicked.png', '.png')
+    img.src = ret
+}
 
 
+function CompetenciaButton(e) {
+    let img = e.firstElementChild;
+    
+    if(!img.classList.contains('w3-ligh-grey-opacity')){
+        //Change to click image
+        //img.src = img.src.replace('.png', 'Clicked.png')
+        var getsrcText = img.alt;
+        img.classList.add('w3-ligh-grey-opacity')
+        competenciatxt.innerHTML = getsrcText
+        for (let i = 0; i < CompetenciaBtn.length; i++) {
+            if (img.src != CompetenciaBtn[i].firstElementChild.src) {
+                if (CompetenciaBtn[i].firstElementChild.classList.contains('w3-ligh-grey-opacity')) {
+                    //Remove the click image from other buttons
+                    //var ret= CompetenciaBtn[i].firstElementChild.src.replace('Clicked', '')
+                    //CompetenciaBtn[i].firstElementChild.src = ret
+                    CompetenciaBtn[i].firstElementChild.classList.remove('w3-ligh-grey-opacity')
+                }
+            }
+            
+        }
+    }
+}
 
+function CompetenciaHoverState() {
+    
+}
 // Automatic Slideshow - change image every 4 seconds
 var myIndex = 0;
 carousel();
@@ -81,30 +145,35 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
+if(firstTerm != null){
+    firstTerm.addEventListener('click', function openTerm() {
+        firstTerm.classList.add('w3-orange')
+        secondTerm.classList.remove('w3-orange')
+        var i;
+        var x = document.getElementsByClassName("term");
+        for (i = 0; i < x.length; i++) {
+          x[i].style.display = "none";
+        }
+        document.getElementById('FirstTerm').style.display = "block";
+      
+    })
+    
+}
 
-firstTerm.addEventListener('click', function openTerm() {
-    firstTerm.classList.add('w3-orange')
-    secondTerm.classList.remove('w3-orange')
-    var i;
-    var x = document.getElementsByClassName("term");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    document.getElementById('FirstTerm').style.display = "block";
-  
-})
+if( secondTerm != null){
+    secondTerm.addEventListener('click', function openTerm() {
+        secondTerm.classList.add('w3-orange')
+        firstTerm.classList.remove('w3-orange')
+        var i;
+        var x = document.getElementsByClassName("term");
+        for (i = 0; i < x.length; i++) {
+          x[i].style.display = "none";
+        }
+        document.getElementById('SecondTerm').style.display = "block";
+      
+    })
+}
 
-secondTerm.addEventListener('click', function openTerm() {
-    secondTerm.classList.add('w3-orange')
-    firstTerm.classList.remove('w3-orange')
-    var i;
-    var x = document.getElementsByClassName("term");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    document.getElementById('SecondTerm').style.display = "block";
-  
-})
 
 function openYear(cityName, button) {
     button.classList.add('w3-orange')
